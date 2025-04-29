9976d8eb42a63e300009eeb94ffd7abf401fdb13
GustavoAlbuq <167881515+GustavoAlbuq@users.noreply.github.com>
2025-04-29 16:16:47 -0300

    Create Disciplina.java

---
+++ api/model/Disciplina.java
@@ -0,0 +1,27 @@
+package br.com.alunoonline.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "disciplina")
+public class Disciplina {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_disciplina")
+    private Long idDisciplina;
+
+    @Column(name = "nome_disciplina", nullable = false)
+    private String nomeDisciplina;
+
+    @ManyToOne
+    //esse join colum é para puxar o id de professor, como chave estrangeira
+    @JoinColumn(name = "PROFESSOR_id_professor", nullable = false)
+    private Professor professor;
+}
