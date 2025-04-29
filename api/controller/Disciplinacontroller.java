f57f4635a6f23bc81a5daf7216bbb7e412dd1695
GustavoAlbuq <167881515+GustavoAlbuq@users.noreply.github.com>
2025-04-29 16:15:40 -0300

    Create Disciplinacontroller.java

---
+++ api/controller/Disciplinacontroller.java
@@ -0,0 +1,30 @@
+package br.com.alunoonline.api.controller;
+
+import br.com.alunoonline.api.model.Disciplina;
+import br.com.alunoonline.api.service.DisciplinaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/disciplinas")
+public class DisciplinaController {
+
+    @Autowired
+    DisciplinaService disciplinaService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void criarDisciplina(@RequestBody Disciplina disciplina){
+        //disciplina minusculo se torna java a partir daqui
+        disciplinaService.criarDisciplina(disciplina);
+    }
+
+    @GetMapping("/{idProfessor}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Disciplina> listaDisciplinaPorIdProfessor(@PathVariable Long idProfessor){
+        return disciplinaService.listarDisciplinaPorIdProfessor(idProfessor);
+    }
+}
